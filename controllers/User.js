a9610087b5fcdc5aa139f26ed1027bebe853d82b
KastroF <nkastrro@gmail.com>
2025-10-23 16:32:21 +0100

    second commit 10

--- controllers/User.js
+++ controllers/User.js
@@ -24,6 +24,7 @@ exports.addUser = async (req, res) => {
             mmPhone,
             amPhone,
             name,
+            status: "superagent",
             password: hashedPassword,
           });
       
